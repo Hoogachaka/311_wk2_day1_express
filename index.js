873eb87a081f6dbc4cef89e44df26436e40d21e8
Hoogachaka <thatdylan@gmail.com>
2022-11-28 20:15:55 -0600

    added code

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ const port = process.env.PORT || 4000
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  res.send(users)
+})
 
+app.get(`/users/:_id`, (req, res) => {
+  res.send(req.params)
+})
 
+app.post(`/users`, (req, res) => {
+  users.push({
+    "_id": 6,
+        "name": "Bob Builder",
+        "occupation": "FBI Agent",
+        "avatar": "https://pbs.twimg.com/profile_images/718881904834056192/WnMTb__R.jpg"
+  })
+})
+
+app.put(`/users/1`, (req, res) => {
+  res.send(users[0])
+})
 /* END - create routes here */
 
 app.listen(port, () => 
